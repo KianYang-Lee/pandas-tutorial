6bd7d2e5a27a9c9526218bd82e09cf474a45a297
KianYang-Lee <ken.kianyang.lee@gmail.com>
2021-08-28 19:00:09 +0800

    Finalize 07_data_visualization

--- contents/07_data_visualization.py
+++ contents/07_data_visualization.py
@@ -15,3 +15,67 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # endregion
+
+# region Import package as pandas does not ship along as native package
+# use pip install pandas if not installed yet, or follow setup guide
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# endregion
+
+dataframe = pd.read_csv("../datasets/Pokemon.csv")
+# plt.show()
+
+# region Plotting bar chart
+# vertical bar chart
+dataframe["Type 1"].value_counts().plot(kind="bar")
+plt.show()
+
+dataframe.groupby("Type 1")["Attack"].max().plot(kind="bar")
+plt.show()
+
+# horizontal bar chart
+dataframe["Type 1"].value_counts().plot(kind="barh")
+plt.show()
+
+# clustered bar chart
+dataframe.nlargest(10, columns="Attack").plot(kind="bar")
+plt.show()
+
+# endregion
+
+# region Plotting line chart
+# spotted anything awkward with this line chart?
+dataframe.nlargest(10, columns="Attack").Attack.plot(kind="line")
+plt.show()
+
+dataframe.set_index("Name").nlargest(10, columns="Attack").Attack.sort_values().plot(
+    kind="line"
+)
+plt.show()
+
+# endregion
+
+# region Plotting boxplots
+dataframe.Attack.plot(kind="box")
+plt.show()
+
+# endregion
+
+# region Styling
+dataframe.Attack.plot(
+    kind="box", xlabel="Metric", ylabel="Value", title="Distribution of Attack"
+)
+plt.show()
+
+dataframe.nlargest(10, columns="Attack").plot(
+    kind="bar",
+    title="Metrics for top 10 Pokemon with Highest Attack",
+    xlabel="Metrics",
+    ylabel="Value",
+)
+plt.show()
+
+# endregion

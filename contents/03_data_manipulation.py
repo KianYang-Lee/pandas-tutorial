f80ee09d6358cb6209e7502dfa52b5553ea82c54
KianYang-Lee <ken.kianyang.lee@gmail.com>
2021-08-28 15:41:49 +0800

    Finalized 03_data_manipulation

--- contents/03_data_manipulation.py
+++ contents/03_data_manipulation.py
@@ -15,3 +15,90 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # endregion
+
+# region Import package as pandas does not ship along as native package
+# use pip install pandas if not installed yet, or follow setup guide
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# endregion
+
+dataframe = pd.read_csv("../datasets/Pokemon.csv")
+# print(dataframe.head())
+# print(dataframe.describe())
+# print(dataframe["Type 1"].value_counts())
+# dataframe["Type 1"].value_counts().plot(kind="barh")
+# plt.show()
+
+# region Every DataFrame at least has one row index and
+# one column index
+dataframe.columns
+dataframe.index
+
+# endregion
+
+# region Accessing column(s)
+# dot notation to access attribute of object
+dataframe.Name
+
+# similar operation but using indexing operator - square bracket
+dataframe["Name"]
+
+# select multiple columns
+dataframe[["Name", "HP", "Attack", "Defense"]]
+
+# drilling down
+dataframe["Name"][0]
+# endregion
+
+# region Indexing
+# index-based selection - based on index position
+# select first row
+dataframe.iloc[1, :]
+
+# select last column
+dataframe.iloc[:, -1]
+
+# label-based selection - based on index value
+# select Name column
+dataframe.loc[:, "Name"]
+
+# select second row
+dataframe.loc[1, :]
+
+# select multiple columns
+dataframe.loc[:, ["Name", "HP", "Attack", "Defense"]]
+
+# endregion
+
+# region Index can be changes, like everything else in life
+df_with_name_index = dataframe.set_index("Name", drop=True)
+df_with_name_index.loc["Charizard"]
+
+# endregion
+
+# region Conditional selection that fulfills certain criteria
+# setting a condition to return a Boolean Series
+min_HP_condition = dataframe.HP > 200
+
+# this line will not work, do you know why?
+# dataframe.iloc[min_HP_condition]
+
+# conditional selection of rows that satisfies the condition
+dataframe.loc[min_HP_condition]
+
+# conditional selection on multiple criteria
+min_attack_condition = dataframe.Attack > 100
+min_defense_condition = dataframe.Defense > 100
+dataframe.loc[min_attack_condition & min_defense_condition]
+dataframe.loc[min_attack_condition | min_defense_condition]
+
+# endregion
+
+# region Changing a value
+# able to access specific value in the DataFrame means that
+# you can also change the value
+dataframe.loc[0, "Name"] = "Rilakuma"
+
+# endregion

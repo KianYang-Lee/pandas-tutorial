30e3ef767131e2d5fadef56ad049f0b263b9f965
KianYang-Lee <ken.kianyang.lee@gmail.com>
2021-08-28 18:31:05 +0800

    Finalize 06_data_analysis

--- contents/06_data_analysis.py
+++ contents/06_data_analysis.py
@@ -15,3 +15,45 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # endregion
+
+# region Import package as pandas does not ship along as native package
+# use pip install pandas if not installed yet, or follow setup guide
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# endregion
+
+dataframe = pd.read_csv("../datasets/Pokemon.csv")
+# print(dataframe.head())
+# print(dataframe.describe())
+# print(dataframe["Type 1"].value_counts())
+# dataframe["Type 1"].value_counts().plot(kind="barh")
+# plt.show()
+
+# region Analysing by ascending/descending order
+dataframe.sort_values(by="Attack", ascending=False)
+dataframe.sort_values(by="Attack", ascending=True)
+# endregion
+
+# region Analysing by Single Group
+dataframe.groupby("Type 1").head()
+dataframe.groupby("Type 1").count()
+dataframe.groupby("Type 1").HP.mean().sort_values(ascending=False)
+dataframe.groupby("Type 1").HP.median().sort_values(ascending=False)
+dataframe.groupby("Type 1").HP.max().sort_values(ascending=False)
+
+# GroupByObject can be thought as a resemblance to DataFrame
+# and certain functions can be applied on it by using apply method
+dataframe.groupby("Type 1").apply(lambda x: x.loc[:, "Name"])
+dataframe.groupby("Type 1").apply(np.sum, axis=0)
+
+# endregion
+
+# region Analysing by Multiple Groups
+dataframe.groupby(["Type 1", "Type 2"]).head()
+dataframe.groupby(["Type 1", "Type 2"]).Defense.describe()
+dataframe.groupby(["Type 1", "Type 2"]).HP.mean().sort_values(ascending=True)
+
+# endregion
